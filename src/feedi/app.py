bf088dd51430e6df3c9c6fbf51019711e0ca7ae2
facundoolano <facundo.olano@gmail.com>
2023-07-15 21:45:16 -0300

    render as template

--- src/feedi/app.py
+++ src/feedi/app.py
@@ -1,35 +1,37 @@
 import os
 
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 GOODREADS_TOKEN = os.getenv("GOODREADS_TOKEN")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-HARDCODED_FEEDS = [
-    "https://facundoolano.github.io/feed.xml",
-    "https://mastodon.gamedev.place/@grumpygamer.rss",
-    "https://lobste.rs/rss",
-    f"https://github.com/facundoolano.private.atom?token={GITHUB_TOKEN}",
-    "https://www.ambito.com/rss/pages/home.xml",
-    f"https://www.goodreads.com/user/updates_rss/19714153?key={GOODREADS_TOKEN}"
-]
+HARDCODED_FEEDS = {
+    "Apuntes Inchequeables": "https://facundoolano.github.io/feed.xml",
+    "@grumpygamer": "https://mastodon.gamedev.place/@grumpygamer.rss",
+    "lobset.rs": "https://lobste.rs/rss",
+    "Github": f"https://github.com/facundoolano.private.atom?token={GITHUB_TOKEN}",
+    "ambito.com": "https://www.ambito.com/rss/pages/home.xml",
+    "Goodreads": f"https://www.goodreads.com/user/updates_rss/19714153?key={GOODREADS_TOKEN}"
+}
 
 
 @app.route("/")
 def hello_world():
 
-    all_entries = []
-    for source in HARDCODED_FEEDS:
-        feed = feedparser.parse(source)
+    entries = []
+    for feed_name, url in HARDCODED_FEEDS.items():
+        feed = feedparser.parse(url)
         for entry in feed['entries']:
-            all_entries.append({'title': entry.get('title', '[no title]'),
-                                'url': entry['link'],
-                                'body': entry['summary'],
-                                'date': entry['published_parsed']})
+            entries.append({'feed': feed_name,
+                            'title': entry.get('title', '[no title]'),
+                            'url': entry['link'],
+                            'body': entry['summary'],
+                            'date': entry['published'],
+                            'date_parsed': entry['published_parsed']})
 
-    all_entries.sort(key=lambda e: e['date'], reverse=True)
+    entries.sort(key=lambda e: e['date_parsed'], reverse=True)
 
-    return "<p>Hello, World!</p>"
+    return render_template('base.html', entries=entries)
