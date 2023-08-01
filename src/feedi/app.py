de86237cb45eed601632feda81021da0aa0917f5
facundoolano <facundo.olano@gmail.com>
2023-08-01 12:55:33 -0300

    extract filters module

--- src/feedi/app.py
+++ src/feedi/app.py
@@ -1,14 +1,10 @@
 # coding: utf-8
 
-import datetime
 import logging
 import os
-import urllib
 
 import click
 import flask
-import lxml
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 import feedi.parser as parser
@@ -58,36 +54,5 @@ def create_app():
     @click.argument('feed-name')
     def delete_feed(feed_name):
         parser.delete_feed(app, feed_name)
-
-    # FIXME move somewhere else
-    # TODO unit test this
-    @app.template_filter('humanize')
-    def humanize_date(dt):
-        delta = datetime.datetime.utcnow() - dt
-
-        if delta < datetime.timedelta(seconds=60):
-            return f"{delta.seconds}s"
-        elif delta < datetime.timedelta(hours=1):
-            return f"{delta.seconds // 60}m"
-        elif delta < datetime.timedelta(days=1):
-            return f"{delta.seconds // 60 // 60 }h"
-        elif delta < datetime.timedelta(days=8):
-            return f"{delta.days}d"
-        elif delta < datetime.timedelta(days=365):
-            return dt.strftime("%b %d")
-        return dt.strftime("%b %d, %Y")
-
-    @app.template_filter('feed_domain')
-    def feed_domain(feed):
-        parts = urllib.parse.urlparse(feed.url or feed.server_url)
-        return f'{parts.scheme}://{parts.netloc}'
-
-    @app.template_filter('sanitize')
-    def sanitize_content(html):
-        # poor man's line truncating: reduce the amount of characters and let bs4 fix the html
-        if len(html) > 500:
-            html = html[:500] + '…'
-            html = str(BeautifulSoup(html, 'lxml'))
-        return html
 
     return app
---
+++ src/feedi/filters.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+import datetime
+import urllib
+
+from bs4 import BeautifulSoup
+from flask import current_app as app
+
+
+# TODO unit test this
+@app.template_filter('humanize')
+def humanize_date(dt):
+    delta = datetime.datetime.utcnow() - dt
+
+    if delta < datetime.timedelta(seconds=60):
+        return f"{delta.seconds}s"
+    elif delta < datetime.timedelta(hours=1):
+        return f"{delta.seconds // 60}m"
+    elif delta < datetime.timedelta(days=1):
+        return f"{delta.seconds // 60 // 60 }h"
+    elif delta < datetime.timedelta(days=8):
+        return f"{delta.days}d"
+    elif delta < datetime.timedelta(days=365):
+        return dt.strftime("%b %d")
+    return dt.strftime("%b %d, %Y")
+
+
+@app.template_filter('feed_domain')
+def feed_domain(feed):
+    parts = urllib.parse.urlparse(feed.url or feed.server_url)
+    return f'{parts.scheme}://{parts.netloc}'
+
+
+@app.template_filter('sanitize')
+def sanitize_content(html):
+    # poor man's line truncating: reduce the amount of characters and let bs4 fix the html
+    if len(html) > 500:
+        html = html[:500] + '…'
+        html = str(BeautifulSoup(html, 'lxml'))
+    return html
