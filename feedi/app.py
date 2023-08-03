68f3025b9f4a212253111b69e7e23a1ceaf48bee
Facundo Olano <facundo.olano@gmail.com>
2023-08-03 14:52:17 -0300

    periodic tasks (#2)
    * experiment using gevent and huey for periodic tasks
    
    * experiment with feed fetch parallelization
    
    * add some comments
    
    * improve logging
    
    * prevent huey double init in dev server
    
    * move task setup to decorator
    
    * put sync feed cron in config
    
    * refactor for readability
    
    * comment

--- feedi/app.py
+++ feedi/app.py
@@ -1,8 +1,10 @@
 # coding: utf-8
 
+from gevent import monkey; monkey.patch_all()
 import logging
 
 import flask
+from werkzeug.serving import is_running_from_reloader
 
 from feedi.models import db
 
@@ -12,7 +14,7 @@ def create_app():
     app.config.from_object('feedi.config')
     app.config.from_envvar('FEEDI_CONFIG', silent=True)
 
-    app.logger.setLevel(logging.DEBUG)
+    app.logger.setLevel(logging.INFO)
 
     db.init_app(app)
 
@@ -21,8 +23,30 @@ def create_app():
 
         from . import filters, routes, tasks
 
+        if not is_running_from_reloader():
+            # we want only one huey scheduler running, so we make sure
+            # this isn't the dev server reloader process
+
+            # btw this may not be the right place to put the huey startup
+            # perhaps it should be in wsgi, but we wouldn't have it in dev server
+            tasks.huey.start()
+
     @app.teardown_appcontext
     def shutdown_session(exception=None):
         db.session.remove()
+
+    return app
+
+
+def create_huey_app():
+    """
+    Construct a minimal flask app for exposing to the huey tasks.
+    This is necessary to make config and db session available to the periodic tasks.
+    """
+    app = flask.Flask('huey_app')
+    app.config.from_object('feedi.config')
+    app.config.from_envvar('FEEDI_CONFIG', silent=True)
+    app.logger.setLevel(logging.INFO)
+    db.init_app(app)
 
     return app
--- feedi/tasks.py
+++ feedi/tasks.py
@@ -1,37 +1,83 @@
 # coding: utf-8
+"""
+This module contains tasks that can be scheduled by huey and/or run as flask cli commands.
+(The cli commands could eventually be moved to another module).
+"""
 
 import csv
 import datetime
 import json
+from functools import wraps
 
 import click
 import flask
 import sqlalchemy.dialects.sqlite as sqlite
 from flask import current_app as app
+from huey import crontab
+from huey.contrib.mini import MiniHuey
 
 import feedi.models as models
 import feedi.sources as sources
+from feedi.app import create_huey_app
 from feedi.models import db
 
 feed_cli = flask.cli.AppGroup('feed')
 
+huey = MiniHuey()
+
+def huey_task(*huey_args):
+    "Wraps a function to make a it a MiniHuey task that is run inside a flask app context."
+
+    huey_decorator = huey.task(*huey_args)
+
+    def with_context(f):
+
+        @wraps(f)
+        def decorator(*args, **kwargs):
+            # run the task inside an app context and log start and finish
+            app = create_huey_app()
+            with app.app_context():
+                fargs = ' '.join(args)
+                fkwargs = ' '.join([f'{k}={v}' for (k, v) in kwargs.items()])
+                app.logger.info("STARTING %s %s %s", f.__name__, fargs, fkwargs)
+
+                f(*args, **kwargs)
+
+                app.logger.info("FINISHED %s %s %s", f.__name__, fargs, fkwargs)
+
+        return decorator
+
+    def composed_decorator(f):
+        return huey_decorator(with_context(f))
+
+    return composed_decorator
+
+
 @feed_cli.command('sync')
+@huey_task(crontab(minute=app.config['SYNC_FEEDS_CRON_MINUTES']))
 def sync_all_feeds():
-    db_feeds = db.session.execute(db.select(models.Feed)).all()
-    for (db_feed,) in db_feeds:
-        if db_feed.type == models.Feed.TYPE_RSS:
-            sync_rss_feed(db_feed)
-        elif db_feed.type == models.Feed.TYPE_MASTODON_ACCOUNT:
-            sync_mastodon_feed(db_feed)
+    feeds = db.session.execute(db.select(models.Feed.name, models.Feed.type)).all()
+
+    tasks = []
+    for feed in feeds:
+        if feed.type == models.Feed.TYPE_RSS:
+            task = sync_rss_feed(feed.name)
+        elif feed.type == models.Feed.TYPE_MASTODON_ACCOUNT:
+            task = sync_mastodon_feed(feed.name)
         else:
-            app.logger.error("unknown feed type %s", db_feed.type)
+            app.logger.error("unknown feed type %s", feed.type)
             continue
 
-        db.session.commit()
+        tasks.append(task)
+
+    # wait for concurrent tasks to finish before returning
+    for task in tasks:
+        task.get()
 
 
-def sync_mastodon_feed(db_feed):
-
+@huey_task()
+def sync_mastodon_feed(feed_name):
+    (db_feed, ) = db.session.execute(db.select(models.Feed).filter_by(name=feed_name)).first()
     latest_entry = db_feed.entries.order_by(models.Entry.remote_updated.desc()).first()
     args = {}
     if latest_entry:
@@ -40,54 +86,62 @@ def sync_mastodon_feed(db_feed):
         args['newer_than'] = latest_entry.remote_id
     else:
         # if there isn't any entry yet, get the "first page" of toots from the timeline
-        # TODO make constant/config
         args['limit'] = app.config['MASTODON_FETCH_LIMIT']
 
-    app.logger.info("Fetching toots %s", args)
+    app.logger.debug("Fetching toots %s", args)
     toots = sources.mastodon.fetch_toots(server_url=db_feed.server_url,
                                          access_token=db_feed.access_token,
                                          **args)
-    utcnow = datetime.datetime.utcnow()
-    for values in toots:
-        # upsert to handle already seen entries.
-        # updated time set explicitly as defaults are not honored in manual on_conflict_do_update
-        values['updated'] = utcnow
-        values['feed_id'] = db_feed.id
-        db.session.execute(
-            sqlite.insert(models.Entry).
-            values(**values).
-            on_conflict_do_update(("feed_id", "remote_id"), set_=values)
-        )
+    upsert_entries(db_feed.id, toots)
 
 
-def sync_rss_feed(db_feed):
+@huey_task()
+def sync_rss_feed(feed_name):
+    (db_feed, ) = db.session.execute(db.select(models.Feed).filter_by(name=feed_name)).first()
     utcnow = datetime.datetime.utcnow()
 
     if db_feed.last_fetch and utcnow - db_feed.last_fetch < datetime.timedelta(minutes=app.config['RSS_SKIP_RECENTLY_UPDATED_MINUTES']):
         app.logger.info('skipping recently synced feed %s', db_feed.name)
         return
 
-    app.logger.info('fetching %s', db_feed.name)
-    entries, feed_data, etag, modified,  = sources.rss.fetch(db_feed.url,
-                                                             db_feed.last_fetch,
-                                                             app.config['RSS_SKIP_OLDER_THAN_DAYS'],
-                                                             etag=db_feed.etag, modified=db_feed.modified_header)
+    app.logger.debug('fetching rss %s %s', db_feed.name, db_feed.url)
+    entries, feed_data, etag, modified, = sources.rss.fetch(db_feed.url,
+                                                            db_feed.last_fetch,
+                                                            app.config['RSS_SKIP_OLDER_THAN_DAYS'],
+                                                            etag=db_feed.etag, modified=db_feed.modified_header)
 
     db_feed.last_fetch = utcnow
     db_feed.etag = etag
     db_feed.modified_header = modified
     db_feed.raw_data = json.dumps(feed_data)
+    db.session.merge(db_feed)
+    db.session.commit()
 
-    for entry_values in entries:
+    upsert_entries(db_feed.id, entries)
+
+
+
+# NOTE most of this code should probably live in a db related module eg models
+def upsert_entries(feed_id, entries_values):
+    """
+    Insert or update the given feed's entries based on the values in the list.
+    An insert and commit is emitted for each entry.
+    """
+    utcnow = datetime.datetime.utcnow()
+    for values in entries_values:
         # upsert to handle already seen entries.
         # updated time set explicitly as defaults are not honored in manual on_conflict_do_update
-        entry_values['updated'] = utcnow
-        entry_values['feed_id'] = db_feed.id
+        values['updated'] = utcnow
+        values['feed_id'] = feed_id
         db.session.execute(
             sqlite.insert(models.Entry).
-            values(**entry_values).
-            on_conflict_do_update(("feed_id", "remote_id"), set_=entry_values)
+            values(**values).
+            on_conflict_do_update(("feed_id", "remote_id"), set_=values)
         )
+
+        # inline commit to avoid sqlite locking when fetching parallel feeds
+        db.session.commit()
+
 
 @feed_cli.command('debug')
 @click.argument('url')
@@ -98,6 +152,8 @@ def debug_feed(url):
 @feed_cli.command('load')
 @click.argument("file")
 def create_test_feeds(file):
+    "Load feeds from a local csv file."
+
     with open(file) as csv_file:
         for attrs in csv.reader(csv_file):
             feed_type = attrs[0]
@@ -136,6 +192,8 @@ def create_test_feeds(file):
 @feed_cli.command('delete')
 @click.argument('feed-name')
 def delete_feed(feed_name):
+    "Remove a feed and its entries from the database."
+
     query = db.delete(models.Feed).where(models.Feed.name == feed_name)
     db.session.execute(query)
     db.session.commit()
