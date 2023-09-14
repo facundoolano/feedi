c14f32870ccf54b99c00ed3e3552d6fd57208e36
facundoolano <facundo.olano@gmail.com>
2023-09-14 16:24:23 -0300

    experiment with headless browser scraping

--- feedi/extract_article.js
+++ feedi/extract_article.js
@@ -5,11 +5,31 @@
 
 const { JSDOM } = require("jsdom");
 const { Readability } = require('@mozilla/readability');
+const timers = require('node:timers/promises');
 
-const url = process.argv[2];
-
-JSDOM.fromURL(url).then(function (dom) {
+function parseAndPrint(dom) {
   let reader = new Readability(dom.window.document);
   let article = reader.parse();
   process.stdout.write(JSON.stringify(article));
-});
+  process.exit()
+}
+
+async function fetchWithPuppeteer(url) {
+  const puppeteer = require('puppeteer');
+  const browser = await puppeteer.launch({headless: "new"});
+  const page = await browser.newPage();
+  await page.goto(url);
+  await timers.setTimeout(1000);
+
+  const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+  return new JSDOM(data);
+}
+
+// FIXME  implement cleaner arg parsing
+const url = process.argv[2];
+
+if (process.argv[3] === 'puppet') {
+  fetchWithPuppeteer(url).then(parseAndPrint);
+} else {
+  JSDOM.fromURL(url).then(parseAndPrint);
+}
