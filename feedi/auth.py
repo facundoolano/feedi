8c2c941998c1bb2763b9c0c06b09b2fc73c7feaa
facundoolano <facundo.olano@gmail.com>
2023-11-24 14:13:07 -0300

    add mastodon oauth flow routes

--- feedi/auth.py
+++ feedi/auth.py
@@ -1,3 +1,5 @@
+import urllib
+
 import flask
 import flask_login
 from flask import current_app as app
@@ -5,6 +7,7 @@ from flask_login import current_user, login_required
 
 import feedi.models as models
 from feedi.models import db
+from feedi.parsers import mastodon
 
 
 def init():
@@ -63,3 +66,69 @@ def kindle_add_submit():
     models.KindleDevice.add_from_url(current_user.id, verifier, redirect_url)
     db.session.commit()
     return flask.redirect(flask.url_for('entry_list'))
+
+
+@app.get("/auth/mastodon")
+@login_required
+def mastodon_oauth():
+    return flask.render_template('mastodon.html')
+
+
+@app.post("/auth/mastodon")
+@login_required
+def mastodon_oauth_submit():
+    """TODO"""
+    # sanitize base url
+    base_url = flask.request.form.get('url')
+    if not base_url:
+        return flask.render_template('mastodon.html', error_msg="The instance url is required")
+
+    url_parts = urllib.parse.urlparse(base_url)
+    base_url = f'https://{url_parts.netloc}'
+
+    # if not already registered, register with mastopy and save to db
+    masto_app = db.session.scalar(db.select(models.MastodonApp).filter_by(api_base_url=base_url))
+    if not masto_app:
+        client_id, client_secret = mastodon.register_app(base_url)
+        masto_app = models.MastodonApp(api_base_url=base_url,
+                                       client_id=client_id,
+                                       client_secret=client_secret)
+        db.session.add(masto_app)
+        db.session.commit()
+
+    redirect_url = mastodon.auth_redirect_url(masto_app.api_base_url,
+                                              masto_app.client_id,
+                                              masto_app.client_secret,
+                                              flask.url_for('mastodon_oauth_calback',
+                                                            appid=masto_app.id,
+                                                            _external=True))
+    return flask.redirect(redirect_url)
+
+
+@app.get("/auth/mastodon/callback")
+@login_required
+def mastodon_oauth_calback():
+    """TODO"""
+
+    appid = flask.request.form.get('appid')
+    code = flask.request.form.get('code')
+    if not appid or not code:
+        flask.abort(400)
+
+    masto_app = db.get_or_404(models.MastodonApp, int(appid))
+
+    # mastodon api requires passing again the already used callback url
+    callback_url = flask.url_for('mastodon_oauth_calback',
+                                 appid=masto_app.id,
+                                 _external=True)
+    access_token = mastodon.oauth_login(masto_app.api_base_url, code, callback_url)
+
+    # store the token in the masto accounts table
+    masto_acct = models.MastodonAccount(app_id=masto_app.id,
+                                        user_id=current_user.id,
+                                        access_token=access_token)
+    db.session.add(masto_acct)
+    db.session.commit()
+
+    # redirect to feed creation with masto pre-selected
+    return flask.redirect(flask.url_for('feed_add', masto_acct=masto_acct.id))
