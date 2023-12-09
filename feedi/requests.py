f9aaa582d8e690ab8d64baf3286ada8fb64e9f45
facundoolano <facundo.olano@gmail.com>
2023-12-08 21:54:28 -0300

    enforce a timeout on all requests

--- feedi/requests.py
+++ feedi/requests.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import urllib
 
@@ -7,9 +8,13 @@ from bs4 import BeautifulSoup
 import requests
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
+TIMEOUT_SECONDS = 5
 
 requests = requests.Session()
 requests.headers.update({'User-Agent': USER_AGENT})
+
+# always use a default timeout
+requests.get = functools.partial(requests.get, timeout=TIMEOUT_SECONDS)
 
 logger = logging.getLogger(__name__)
 
