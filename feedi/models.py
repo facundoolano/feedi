47dbb0305c5a4379373ac0d4f48872693e17a4d9
facundoolano <facundo.olano@gmail.com>
2023-12-16 19:26:34 -0300

    remove score field

--- feedi/models.py
+++ feedi/models.py
@@ -228,8 +228,6 @@ class Feed(db.Model):
                                          doc="The original feed data received from the feed, as JSON"))
 
     folder = sa.Column(sa.String, index=True)
-    score = sa.Column(sa.Integer, default=0, nullable=False,
-                      doc="counts how many times articles of this feed have been interacted with. ")
 
     __mapper_args__ = {'polymorphic_on': type,
                        'polymorphic_identity': 'feed'}
