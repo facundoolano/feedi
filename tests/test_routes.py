8729353fd3e74f9b6ce824e882267efadb63c109
facundoolano <facundo.olano@gmail.com>
2023-12-27 19:08:19 -0300

    test fixture setup

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,29 @@
 import pytest
+from feedi.app import create_app
+from feedi.models import db
 
 
-def test_hello():
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///feedi.test.db"
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def test_feed_add(client):
     assert 1 == 1, "1 equals 1"
