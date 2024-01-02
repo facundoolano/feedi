fd366b89b510d161e8fad3fa3b9f767e04c0bf0f
facundoolano <facundo.olano@gmail.com>
2024-01-02 17:53:12 -0300

    accept stdin html in extract_article.js

--- feedi/extract_article.js
+++ feedi/extract_article.js
@@ -13,9 +13,19 @@ function parseAndPrint(dom) {
   process.stdout.write(JSON.stringify(article), process.exit);
 }
 
+async function read(stream) {
+  const chunks = [];
+  for await (const chunk of stream) chunks.push(chunk);
+  return Buffer.concat(chunks).toString('utf8');
+}
+
 const {values, positionals} =  util.parseArgs({
   allowPositionals: true
 });
 const url = positionals[0];
 
-JSDOM.fromURL(url).then(parseAndPrint);
+if (url) {
+  JSDOM.fromURL(url).then(parseAndPrint);
+} else {
+  read(process.stdin).then(s => new JSDOM(s)).then(parseAndPrint);
+}
