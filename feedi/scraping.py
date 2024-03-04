8547bcb1bd3ab2d8461a107ac22fab4bb4419780
Facundo Olano <facundo.olano@gmail.com>
2024-03-04 20:34:07 -0300

    don't crash if embedded anchors are missing href (#95)

--- feedi/scraping.py
+++ feedi/scraping.py
@@ -89,7 +89,7 @@ def all_meta(soup):
 def extract_links(url, html):
     soup = BeautifulSoup(html, 'lxml')
     # checks tag.text so it skips image links
-    links = soup.find_all(lambda tag: tag.name == 'a' and tag.text)
+    links = soup.find_all(lambda tag: tag.name == 'a' and tag.text and 'href' in tag)
     return [(make_absolute(url, a['href']), a.text) for a in links]
 
 
